74dbd9ed6accf7001bd9e1b7268b4b319b3a1d36
Nijjwal Shrestha <nijjwal.shrestha@gmail.com>
2020-03-16 13:01:02 -0600

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+            title: Text(
+              'I Am Rich - App By Gorkha Geeks',
+            ),
+            backgroundColor: Colors.blueGrey[900]),
+        backgroundColor: Colors.blueGrey[900],
+        body: Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
+  );
+}
